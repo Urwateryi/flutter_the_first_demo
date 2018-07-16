5efd283a481598597fcaa90f3154780d7f4fca3f
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-07-16 17:18:29 +0800

    为列表添加图片字段，并优化点击事件

--- lib/home/ComponentBean.dart
+++ lib/home/ComponentBean.dart
@@ -1,6 +1,68 @@
 class ComponentBean {
   String name;
   String pageName;
+  String description;
+  String illustration;
 
-  ComponentBean(this.name, this.pageName);
+  ComponentBean(this.name, this.pageName, this.description, this.illustration);
 }
+
+List<ComponentBean> contentList = <ComponentBean>[
+  new ComponentBean(
+      "Container",
+      "ContainerPage",
+      "包含绘制widget、定位widget、尺寸widget的容器",
+      "assets/images/illustration_1.jpg"),
+  new ComponentBean(
+      "Row", "RowPage", "横向排列内部组件", "assets/images/illustration_2.jpg"),
+  new ComponentBean(
+      "Column", "ColumnPage", "纵向排列内部组件", "assets/images/illustration_3.jpg"),
+  new ComponentBean("RaisedButton", "RaisedButtonPage", "不晓得是啥",
+      "assets/images/illustration_4.jpg"),
+  new ComponentBean(
+      "Scaffold", "ScaffoldPage", "脚手架", "assets/images/illustration_5.jpg"),
+  new ComponentBean("FlutterLogo", "FlutterLogoPage", "不晓得是啥",
+      "assets/images/illustration_6.jpg"),
+  new ComponentBean("Placeholder", "PlaceholderPage", "占位符？？？？",
+      "assets/images/illustration_7.jpg"),
+  new ComponentBean(
+      "Appbar", "AppbarPage", "就是AppBar嘛", "assets/images/illustration_8.jpg"),
+  new ComponentBean(
+      "Text", "TextPage", "文本控件", "assets/images/illustration_9.jpg"),
+  new ComponentBean(
+      "RichText", "TextPage", "文本域控件", "assets/images/illustration_10.jpg"),
+  new ComponentBean(
+      "Image", "TextPage", "图片控件", "assets/images/illustration_11.jpg"),
+  new ComponentBean(
+      "Button", "TextPage", "按钮控件", "assets/images/illustration_12.jpg"),
+  new ComponentBean(
+      "Input", "TextPage", "输入框", "assets/images/illustration_13.jpg"),
+  new ComponentBean(
+      "CheckBox", "TextPage", "单选框", "assets/images/illustration_14.jpg"),
+  new ComponentBean(
+      "RadioGroup", "TextPage", "多选框", "assets/images/illustration_15.jpg"),
+  new ComponentBean(
+      "Progress", "TextPage", "进度条", "assets/images/illustration_16.jpg"),
+  new ComponentBean(
+      "Slide", "TextPage", "控制条", "assets/images/illustration_17.jpg"),
+  new ComponentBean("单个Item的ListView", "TextPage", "每个item都是同一种样式",
+      "assets/images/illustration_18.jpg"),
+  new ComponentBean("多个Item的ListView", "TextPage", "一个列表有多个item的样式",
+      "assets/images/illustration_19.jpg"),
+  new ComponentBean(
+      "Stack", "TextPage", "层叠布局", "assets/images/illustration_20.jpg"),
+  new ComponentBean(
+      "Card", "TextPage", "卡片布局", "assets/images/illustration_21.jpg"),
+  new ComponentBean(
+      "GridView", "TextPage", "网格布局", "assets/images/illustration_22.jpg"),
+  new ComponentBean("自定义控件", "TextPage", "自定义有状态或者无状态的控件",
+      "assets/images/illustration_23.jpg"),
+  new ComponentBean("缓存", "TextPage", "有没有sharedprenference或者sqlite等",
+      "assets/images/illustration_24.jpg"),
+  new ComponentBean(
+      "网络请求", "TextPage", "网络请求封装", "assets/images/illustration_25.jpg"),
+  new ComponentBean(
+      "工具类", "TextPage", "工具类的封装与使用", "assets/images/illustration_26.jpg"),
+  new ComponentBean(
+      "全局变量", "TextPage", "全局变量的定义与使用", "assets/images/illustration_27.jpg"),
+];
--- lib/home/homepage.dart
+++ lib/home/homepage.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'ComponentBean.dart';
-import 'text/TextPage.dart';
 import 'package:the_first_one/utils/PageUtil.dart';
+import 'text/TextPage.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,55 +11,61 @@ class HomePage extends StatefulWidget {
 }
 
 class HomeState extends State<HomePage> {
-  List<ComponentBean> _contentList = <ComponentBean>[
-    new ComponentBean("Container", "ContainerPage"),
-    new ComponentBean("Row", "RowPage"),
-    new ComponentBean("Column", "ColumnPage"),
-    new ComponentBean("RaisedButton", "RaisedButtonPage"),
-    new ComponentBean("Scaffold", "ScaffoldPage"),
-    new ComponentBean("FlutterLogo", "FlutterLogoPage"),
-    new ComponentBean("Placeholder", "PlaceholderPage"),
-    new ComponentBean("Appbar", "AppbarPage"),
-    new ComponentBean("Text", "TextPage"),
-    new ComponentBean("Image", "TextPage"),
-    new ComponentBean("Button", "TextPage"),
-    new ComponentBean("Input", "TextPage"),
-    new ComponentBean("CheckBox", "TextPage"),
-    new ComponentBean("RadioGroup", "TextPage"),
-    new ComponentBean("Progress", "TextPage"),
-    new ComponentBean("Slide", "TextPage"),
-    new ComponentBean("单个Item的ListView", "TextPage"),
-    new ComponentBean("多个Item的ListView", "TextPage"),
-    new ComponentBean("GridView", "TextPage"),
-    new ComponentBean("自定义控件", "TextPage"),
-    new ComponentBean("缓存", "TextPage"),
-    new ComponentBean("网络请求", "TextPage"),
-    new ComponentBean("工具类", "TextPage"),
-    new ComponentBean("全局变量", "TextPage")
-  ];
-
   Widget _buildRow(int index) {
-    return new ListTile(
-        title: new GestureDetector(
-            child: new Padding(
-                padding: new EdgeInsets.all(10.0),
-                child: new Text(_contentList[index].name)),
-            onTap: () {
-              Fluttertoast.showToast(msg: _contentList[index].name);
-              new PageUtil()
-                  .pushTo(context, new TextPage(_contentList[index].name));
-            }));
+    return new GestureDetector(
+        child: new Container(
+          padding:const EdgeInsets.all(10.0),
+          color: Colors.white,
+          child: new Row(
+            children: <Widget>[
+              new Expanded(
+                  child: new Column(
+                children: <Widget>[
+                  new Container(
+                    alignment: Alignment.centerLeft,
+                    child: new Padding(
+                      padding: new EdgeInsets.all(10.0),
+                      child: new Text(contentList[index].name,
+                          style: new TextStyle(
+                              fontSize: 20.0, color: Colors.black),
+                          maxLines: 1),
+                    ),
+                  ),
+                  new Container(
+                    alignment: Alignment.centerLeft,
+                    child: new Padding(
+                      padding: new EdgeInsets.all(10.0),
+                      child: new Text(contentList[index].description,
+                          style:
+                              new TextStyle(fontSize: 15.0, color: Colors.grey),
+                          maxLines: 2),
+                    ),
+                  )
+                ],
+              )),
+              new Container(
+                  child: new Image.asset(contentList[index].illustration,
+                      width: 120.0, height: 80.0, fit: BoxFit.cover)),
+            ],
+          ),
+        ),
+        onTap: () {
+          new PageUtil().pushTo(context, new TextPage(contentList[index].name));
+        });
   }
 
   Widget _buildList() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: _contentList.length * 2,
-        itemBuilder: (context, position) {
-          if (position.isOdd) return new Divider();
-          final index = position ~/ 2; //整除
-          return _buildRow(index);
-        });
+    return new Container(
+        color: Colors.white,
+        child: new ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemCount: contentList.length * 2,
+            itemBuilder: (context, position) {
+              if (position.isOdd)
+                return new Divider(height: 1.0, color: Colors.grey);
+              final index = position ~/ 2; //整除
+              return _buildRow(index);
+            }));
   }
 
   @override
