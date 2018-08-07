8ea08d01964721b01620f53f2ed4ef38e2dbe968
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-08-07 17:32:27 +0800

    封装dio

---
+++ lib/model/Api.dart
@@ -0,0 +1,4 @@
+class Api {
+  static const String BaseUrl = "http://www.wanandroid.com/";
+
+}
--- lib/utils/NetUtil.dart
+++ lib/utils/NetUtil.dart
@@ -1,10 +1,30 @@
-import 'package:flutter/services.dart';//导入网络请求相关的包
+import 'package:dio/dio.dart';
+import 'package:the_first_one/model/Api.dart';
 
 class NetUtil {
-  static void get() {
+  //get请求
+  static void get(String url, Function callBack,
+      {Map<String, String> params, Function errorCallBack}) async {
+    if (!url.startsWith("http")) {
+      url = Api.BaseUrl + url;
+    }
 
+    if (params != null && params.isNotEmpty) {
+      StringBuffer sb = new StringBuffer("?");
+      params.forEach((key, value) {
+        sb.write("$key" + "=" + "$value" + "&");
+      });
+      String paramStr = sb.toString();
+      paramStr = paramStr.substring(0, paramStr.length - 1);
+      url += paramStr;
+    }
 
+    callBack(await Dio().get(url));
   }
 
-  static void post() {}
+  //post请求
+  static void post(String url, Function callBack,
+      {Map<String, String> params, Function errorCallBack}) async {
+    callBack(await Dio().post(url));
+  }
 }
