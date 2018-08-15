f4bf20bbc70a6fcdfe8191f38507bd3e901e96b4
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-08-15 16:58:36 +0800

    输出List

--- lib/home/collection/list/ListPage.dart
+++ lib/home/collection/list/ListPage.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'PrintListPage.dart';
+import 'package:the_first_one/utils/PageUtil.dart';
 
-List<String> listTypeList = ["输出", "增加", "删除", "排序", "迭代"];
+class ComponentBean {
+  String name;
+  Widget targetWidget;
+
+  ComponentBean(this.name, this.targetWidget);
+}
+
+List<ComponentBean> listTypeList = [
+  ComponentBean("输出", PrintListPage()),
+  ComponentBean("增加", PrintListPage()),
+  ComponentBean("删除", PrintListPage()),
+  ComponentBean("排序", PrintListPage()),
+  ComponentBean("迭代", PrintListPage()),
+];
 
 class ListPage extends StatelessWidget {
   @override
@@ -14,14 +29,19 @@ class ListPage extends StatelessWidget {
         child: ListView.builder(
           itemCount: listTypeList.length,
           itemBuilder: (BuildContext context, int position) {
-            return Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(bottom: 1.0),
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
-              child: Text(
-                listTypeList[position],
-                style: TextStyle(color: Colors.black, fontSize: 20.0),
+            return GestureDetector(
+              onTap: () {
+                PageUtil().pushTo(context, listTypeList[position].targetWidget);
+              },
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(bottom: 1.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Text(
+                  listTypeList[position].name,
+                  style: TextStyle(color: Colors.black, fontSize: 20.0),
+                ),
               ),
             );
           },
